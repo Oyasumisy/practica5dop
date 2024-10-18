51f36023d3166df4563a947ae7382e0940d2179b
Oyasumisy <oyasumi2819@gmail.com>
2024-10-18 10:20:15 +0300

    w

---
+++ practical5dop.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class practical5dop {
+    // Ракітенко Артем КН-21
+    // Доп.Завдання
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        // Введення кількості чисел
+        System.out.print("Кількість чисел: ");
+        int numberOfElements = scanner.nextInt();
+
+        // Введення діапазону чисел
+        System.out.print("Мінімальне значення діапазону: ");
+        double minRange = scanner.nextDouble();
+        System.out.print("Максимальне значення діапазону: ");
+        double maxRange = scanner.nextDouble();
+
+        // Створення списку і обчислення суми та добутку
+        ArrayList<Double> array = new ArrayList<>();
+        double sum = 0, product = 1;
+
+        for (int i = 0; i < numberOfElements; ++i) {
+            double value = minRange + (maxRange - minRange) * random.nextDouble();
+            array.add(value);
+            sum += value;
+            product *= value;
+        }
+
+        // Обчислення середнього арифметичного і геометричного
+        double arithmeticMean = sum / numberOfElements;
+        double geometricMean = Math.pow(product, 1.0 / numberOfElements);
+
+        // Виведення результатів
+        System.out.printf("Середнє арифметичне: %.2f\n", arithmeticMean);
+        System.out.printf("Середнє геометричне: %.2f\n", geometricMean);
+
+        scanner.close();
+    }
+}
